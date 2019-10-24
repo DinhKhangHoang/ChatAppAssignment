c43fef1c1b87b738f44f0823adcf7e69d58cd0d9
Hoang Dinh Khang <hdkhang1504@gmail.com>
2019-10-24 10:14:06 +0700

    Server-login-request

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,9 @@
 import java.net.*;
 import java.io.*;
-import java.util.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
 //import myPackage.AccountJDBC;
+import java.util.*;
+
+//import Server.ClientThread;
 
 import java.text.SimpleDateFormat;
 
@@ -15,13 +14,13 @@ public class Server {
     private SimpleDateFormat theDate; //For displaying time and date
     private int port; //The port to listen on
     private boolean control; //Decides whether to keep going
-    private Map<String, Integer> mapClient;
+    private Map<String, String> mapClient;
     
     Server(int portNumber){
     	this.port = portNumber;
     	theDate = new SimpleDateFormat("h:mma"); //Set the time
         clientList = new ArrayList<ClientThread>();
-        mapClient = new HashMap<String, Integer>();
+        mapClient = new HashMap<String, String>();
     }
     
     public void start() {
@@ -39,8 +38,8 @@ public class Server {
                 if(!control) break;
                 ClientThread newThread = new ClientThread(socket);
                 clientList.add(newThread);
-                newThread.start(); 
-               
+                newThread.start();
+                
             }
             //Closing the server down due to break in loop
             try {
@@ -65,6 +64,17 @@ public class Server {
         catch (IOException e) {
             String message = theDate.format(new Date()) + " Error on new ServerSocket: " + e + "\n";
                 display(message);
+        }
+    }
+    
+    synchronized void remove(String usr) {
+        for(int i = 0; i < clientList.size(); i++) {
+            ClientThread temp = clientList.get(i);
+            //It was found
+            if(temp.username == usr) {
+                clientList.remove(i);
+                return;
+            }
         }
     }
     
@@ -100,37 +110,112 @@ public class Server {
         	System.out.println("Thread is attempting to create Data Streams.");
         	
         	try {
-                Output = new PrintWriter(socket.getOutputStream(),true);
-                Input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                this.Output = new PrintWriter(socket.getOutputStream(),true);
+                this.Input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 Address = socket.getInetAddress().toString();
                 Port = socket.getPort();
+                this.date = new Date().toString();
                 System.out.println("Address is: " + Address);
                 System.out.println("Port is: " + Port);
-                
+                System.out.println("Date: " + date);
                 String packet;
-                // Read the username and check if username is duplicated
+                // read login packet
                 while(true){
                     packet = Input.readLine();
                     String[] seperated = packet.split(" ");
-                    
-                    if(mapClient.get(seperated[0]) != null) {	
-                        display("Username conflict detected.");
-                        Output.println("false");
-                        writeMsg("[SERVER] Username " + username + " is already taken. Please enter another.");
+                    if(seperated.length == 3 && seperated[0].equalsIgnoreCase("login")) {
+                    	String value = mapClient.get(seperated[1]);
+                    	if(value != null) {
+                    		// neu tim thay client trong bang
+                    		if(Integer.parseInt(value.split(":")[1]) > 0) {
+                    			//client da dang nhap roi
+                    			display("Username conflict detected.");
+                                writeMsg("FALSE");
+                                //writeMsg("[SERVER] Username " + username + " is already taken. Please enter another.");
+                    		}
+                    		else {
+                    			//client chua dang nhap + sua port lai trong bang map
+                    			mapClient.replace(seperated[1], Address + ":" + seperated[2]);
+                    			this.P2pPort = Integer.parseInt(seperated[2]);
+                    			this.username = seperated[1];
+                    			break;
+                    		}
+                        }
+                        else {
+                        	// dang nhap lan dau them client vao bang
+                        	mapClient.put(seperated[1], Address + ":" + seperated[2]);
+                        	this.P2pPort = Integer.parseInt(seperated[2]);
+                        	this.username = seperated[1];
+                        	break;
+                        }
                     }
                     else {
-                    	mapClient.put(seperated[0], Integer.parseInt(seperated[1]));
-                    	break;
+                    	//sai cu phap login
+                    	display("Wrong request!");
+                    	writeMsg("FALSE");
                     }
-                    
                 }
-                Output.println("true");
+                // dang nhap thanh cong
+                writeMsg("TRUE");
                 display(username + " just connected.");
             }
             catch (IOException e) {
                     display("Exception creating new Input/output Streams: " + e);
                     return;
             }
+        	
+        }
+        
+        public void run() {
+        	boolean KeepGoing = true;
+        	while(KeepGoing) {
+        		try {
+        			String packet = Input.readLine();
+        			String[] seperated = packet.split(" ");
+        			switch(seperated.length) {
+        			case 1:
+        				if (seperated[0].equalsIgnoreCase("LOGOUT")) {
+        					this.P2pPort = 0;
+        					//set port = 0 offline
+        					mapClient.replace(this.username, "0");
+        					close();
+        					KeepGoing = false;
+        					
+        				}
+        				else {
+        					display("Wrong request!");
+                        	writeMsg("FALSE");
+        				}
+        				break;
+        			case 2:
+        				if (seperated[0].equalsIgnoreCase("GET")) {
+        					if(mapClient.get(seperated[1]) == null)
+        						writeMsg("NOTFOUND");
+        					else if(mapClient.get(seperated[1]) == "0")
+        						writeMsg("Offline");
+        					else {
+        						//tim thay destination client
+        						writeMsg("FOUND" + mapClient.get(seperated[1]));
+        					}
+        				}
+        				else {
+        					display("Wrong request!");
+                        	writeMsg("FALSE");
+        				}
+        				break;
+        			default:
+        				display("Wrong request!");
+        				writeMsg("FALSE");
+        				break;
+        			}
+        		}
+        		catch(IOException e){
+        			display("Exception creating new Input/output Streams: " + e);
+                    return;
+        		}
+        	}
+        	remove(username);
+        	close();
         }
         
         private void close() {
@@ -152,7 +237,7 @@ public class Server {
         // Write the string to the Client Output
         private boolean writeMsg(String toSend) {
             if(!socket.isConnected()){
-                close();
+                //close();
                 return false;
             }
             Output.println(toSend);
